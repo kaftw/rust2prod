9f2bd38660d16ac35ce5a5e0216ca3bf4e0e29e3
John Kuempel <jkuempel@its4logistics.com>
2022-04-13 08:45:16 -0400

    Add Health Check Endpoint

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use actix_web::dev::Server;
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(||
+        {
+            App::new()
+                .route("/health_check", web::get().to(health_check))
+        })
+        .bind("127.0.0.1:8080")?
+        .run();
+
+    Ok(server)
+}
---
+++ tests/health_check.rs
@@ -0,0 +1,24 @@
+
+#[tokio::test]
+async fn health_check_works() {
+    // arrange
+    spawn_app();
+
+    let client = reqwest::Client::new();
+
+    // act
+    let response = client
+        .get("http://127.0.0.1:8080/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = rust2prod::run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+}
