2bc2c407215f51fdfa603c556825dc01bac37d74
John Kuempel <jkuempel@its4logistics.com>
2022-05-23 22:46:51 -0400

    Add Docker infra

--- src/configuration.rs
+++ src/configuration.rs
@@ -3,7 +3,7 @@ use secrecy::{ExposeSecret, Secret};
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16
+    pub application: ApplicationSettings
 }
 
 #[derive(serde::Deserialize)]
@@ -15,16 +15,27 @@ pub struct DatabaseSettings {
     pub database_name: String
 }
 
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String
+}
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
 
     // init our config reader
     let mut settings = config::Config::default();
-    // add config values from a file names 'configuration'.
-    // it will look for any top-level file with an extension
-    // that 'config' knows how to parse: yaml, json, etc.
-    settings.merge(config::File::with_name("configuration"))?;
-    // try to convert the configuration values it read into
-    // our Settings type
+    let base_path = std::env::current_dir()
+        .expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
+    settings.merge(config::File::from(configuration_directory.join("base"))
+        .required(true))?;
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(| _ | "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+    settings.merge(config::File::from(configuration_directory.join(environment.as_str()))
+        .required(true),)?;
     settings.try_into()
 }
 
@@ -43,5 +54,33 @@ impl DatabaseSettings {
             self.password.expose_secret(),
             self.host,
             self.port))
+    }
+}
+
+pub enum Environment {
+    Local,
+    Production
+}
+
+impl Environment {
+    pub fn as_str(&self) -> & 'static str {
+        match self {
+            Environment::Local      => "local",
+            Environment::Production => "production"
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local"         => Ok(Self::Local),
+            "production"    => Ok(Self::Production),
+            other     => Err(format!(
+                "{} is not a supported environment. Use either 'local' or 'production'.",
+                other
+            ))
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::net::TcpListener;
-use sqlx::PgPool;
 use rust2prod::configuration::get_configuration;
 use rust2prod::startup::run;
 use rust2prod::telemetry::{get_subscriber, init_subscriber};
 use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -12,10 +12,11 @@ async fn main() -> std::io::Result<()> {
 
     // panic if we can't read config
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(
-        &configuration.database.connection_string().expose_secret())
-        .await
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port)).expect("Failed to bind address");
+    let listener = TcpListener::bind(format!("{}:{}", configuration.application.host, configuration.application.port))
+        .expect("Failed to bind address");
     run(listener, connection_pool)?.await
 }
