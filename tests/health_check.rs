74bc617b54f05188213c12dae0d5e091a65a5923
John Kuempel <jkuempel@its4logistics.com>
2022-05-25 16:40:21 -0400

    Add form field validation

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -143,3 +143,34 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         );
     }
 }
+
+#[tokio::test]
+async fn subscriptions_returns_400_when_name_and_email_not_set_or_whitespace() {
+    // arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email")
+    ];
+
+    for (body, description) in test_cases {
+        // act
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a 400 Bad Request when the payload was {}.",
+            description
+        );
+    }
+}
