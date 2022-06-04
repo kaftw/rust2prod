ec0a7764f889f298a7e04a60730e3ac68870e917
John Kuempel <jkuempel@its4logistics.com>
2022-06-03 22:32:42 -0400

    Initial implementation of email client

--- src/startup.rs
+++ src/startup.rs
@@ -1,12 +1,15 @@
 use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{App, HttpServer, web};
+use actix_web::web::Data;
 use tracing_actix_web::TracingLogger;
 use sqlx::PgPool;
 use crate::routes::{health_check, subscribe};
+use crate::email_client::EmailClient;
 
-pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, db_pool: PgPool, email_client: EmailClient) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
+    let email_client = Data::new(email_client);
     let server = HttpServer::new(move ||
         {
             App::new()
@@ -15,6 +18,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
                 .route("/subscriptions", web::post().to(subscribe))
                 // register the connection as part of the application state
                 .app_data(db_pool.clone())
+                .app_data(email_client.clone())
         })
         .listen(listener)?
         .run();
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,6 +5,7 @@ use rust2prod::configuration::{DatabaseSettings, get_configuration};
 use rust2prod::startup::run;
 use rust2prod::telemetry::{get_subscriber, init_subscriber};
 use once_cell::sync::Lazy;
+use rust2prod::email_client::EmailClient;
 
 static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
@@ -36,7 +37,14 @@ async fn spawn_app() -> TestApp {
 
     let db_pool = configure_database(&configuration.database).await;
 
-    let server = run(listener, db_pool.clone()).expect("Failed to bind address");
+    let sender_email = configuration.email_client.sender().expect("Invalid sender email address.");
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token
+    );
+
+    let server = run(listener, db_pool.clone(), email_client).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
     TestApp {
