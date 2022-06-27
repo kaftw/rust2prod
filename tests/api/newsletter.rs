66792fb3ed8fdb216b1b02ebf59d4b7817880656
John Kuempel <jkuempel@its4logistics.com>
2022-06-27 16:37:15 -0400

    Implement naïve basic auth

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -125,3 +125,27 @@ async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
 
     app.get_confirmation_links(&email_request)
 }
+
+#[tokio::test]
+async fn requests_missing_authorization_are_rejected() {
+    // arrange
+    let app = spawn_app().await;
+
+    // act
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML</p>"
+            }
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // assert
+    assert_eq!(401, response.status().as_u16());
+    assert_eq!(r#"Basic realm="publish"#, response.headers()["WWW-Authenticate"]);
+}
