87c97bdbc311d8564cfd27492d622de15279ed93
John Kuempel <jkuempel@its4logistics.com>
2022-07-05 13:59:00 -0400

    Store PHC string for password

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -4,6 +4,16 @@ use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::fmt::MakeWriter;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tokio::task::JoinHandle;
+
+pub fn spawn_blocking_with_tracing<F, R>(f: F) -> JoinHandle<R>
+where
+    F: FnOnce() -> R + Send + 'static,
+    R: Send + 'static
+{
+    let current_span = tracing::Span::current();
+    tokio::task::spawn_blocking(move || current_span.in_scope(f))
+}
 
 pub fn get_subscriber<Sink>(
     name: String,
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,3 +1,5 @@
+use argon2::{Algorithm, Argon2, Params, PasswordHasher, Version};
+use argon2::password_hash::SaltString;
 use once_cell::sync::Lazy;
 use rust2prod::configuration::{get_configuration, DatabaseSettings};
 use rust2prod::startup::{get_connection_pool, Application};
@@ -5,7 +7,6 @@ use rust2prod::telemetry::{get_subscriber, init_subscriber};
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use wiremock::MockServer;
-use sha3::Digest;
 
 static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
@@ -34,10 +35,15 @@ impl TestUser {
         }
     }
     async fn store(&self, pool: &PgPool) {
-        let password_hash = sha3::Sha3_256::digest(
-            self.password.as_bytes()
-        );
-        let password_hash = format!("{:x}", password_hash);
+        let salt = SaltString::generate(&mut rand::thread_rng());
+        let password_hash = Argon2::new(
+            Algorithm::Argon2id,
+            Version::V0x13,
+            Params::new(15000, 2, 1, None).unwrap())
+            .hash_password(self.password.as_bytes(), &salt)
+            .unwrap()
+            .to_string();
+
         sqlx::query!(
             "INSERT INTO users(user_id, username, password_hash) \
             VALUES($1, $2, $3)",
@@ -56,7 +62,7 @@ pub struct TestApp {
     pub db_pool: PgPool,
     pub email_server: MockServer,
     pub port: u16,
-    test_user: TestUser
+    pub(crate) test_user: TestUser
 }
 
 pub struct ConfirmationLinks {
@@ -72,7 +78,7 @@ impl TestApp {
             .body(body)
             .send()
             .await
-            .expect("Failted to execute request.")
+            .expect("Failed to execute request.")
     }
 
     pub async fn confirm(&self, subscription_token: String) -> reqwest::Response {
