127fcf1b9e59afab2228cf68a4eccd7ba03fdc01
John Kuempel <jkuempel@its4logistics.com>
2022-07-06 17:36:22 -0400

    Use flash cookie instead of HMAC tag; Add login integration test

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -134,6 +134,20 @@ impl TestApp {
             .await
             .expect("Failed to execute request.")
     }
+
+    pub async fn post_login<Body>(&self, body: &Body) -> reqwest::Response
+        where Body: serde::Serialize
+    {
+        reqwest::Client::builder()
+            .redirect(reqwest::redirect::Policy::none())
+            .build()
+            .unwrap()
+            .post(&format!("{}/login", &self.address))
+            .form(body)
+            .send()
+            .await
+            .expect("Failed to execute request")
+    }
 }
 
 pub async fn spawn_app() -> TestApp {
@@ -192,4 +206,9 @@ async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .await
         .expect("Failed to migrate database");
     connection_pool
+}
+
+pub fn assert_is_redirect_to(response: &reqwest::Response, location: &str) {
+    assert_eq!(response.status().as_u16(), 303);
+    assert_eq!(response.headers().get("Location").unwrap(), location);
 }
