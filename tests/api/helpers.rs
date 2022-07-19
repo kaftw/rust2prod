ebebf0c2850355890dcf1e41a21d7a9f2f388fce
John Kuempel <jkuempel@its4logistics.com>
2022-07-19 16:10:07 -0400

    Refactor authentication failure flash cookie handling

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -62,6 +62,7 @@ pub struct TestApp {
     pub db_pool: PgPool,
     pub email_server: MockServer,
     pub port: u16,
+    pub api_client: reqwest::Client,
     pub(crate) test_user: TestUser
 }
 
@@ -72,7 +73,7 @@ pub struct ConfirmationLinks {
 
 impl TestApp {
     pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
-        reqwest::Client::new()
+        self.api_client
             .post(&format!("{}/subscriptions", &self.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(body)
@@ -82,7 +83,7 @@ impl TestApp {
     }
 
     pub async fn confirm(&self, subscription_token: String) -> reqwest::Response {
-        reqwest::Client::new()
+        self.api_client
             .get(
                 &format!("{}/subscriptions/confirm?subscription_token={}",
                          &self.address,
@@ -126,7 +127,7 @@ impl TestApp {
     }
 
     pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
-        reqwest::Client::new()
+        self.api_client
             .post(&format!("{}/newsletters", &self.address))
             .basic_auth(&self.test_user.username, Some(&self.test_user.password))
             .json(&body)
@@ -138,15 +139,23 @@ impl TestApp {
     pub async fn post_login<Body>(&self, body: &Body) -> reqwest::Response
         where Body: serde::Serialize
     {
-        reqwest::Client::builder()
-            .redirect(reqwest::redirect::Policy::none())
-            .build()
-            .unwrap()
+        self.api_client
             .post(&format!("{}/login", &self.address))
             .form(body)
             .send()
             .await
             .expect("Failed to execute request")
+    }
+
+    pub async fn get_login_html(&self) -> String {
+        self.api_client
+            .get(&format!("{}/login", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+            .text()
+            .await
+            .unwrap()
     }
 }
 
@@ -172,12 +181,18 @@ pub async fn spawn_app() -> TestApp {
     let port = application.port();
     let address = format!("http://127.0.0.1:{}", port);
     let _ = tokio::spawn(application.run_until_stopped());
+    let api_client = reqwest::Client::builder()
+        .redirect(reqwest::redirect::Policy::none())
+        .cookie_store(true)
+        .build()
+        .unwrap();
 
     let test_app = TestApp {
         address,
         db_pool: get_connection_pool(&configuration.database),
         email_server,
         port,
+        api_client,
         test_user: TestUser::generate()
     };
 
