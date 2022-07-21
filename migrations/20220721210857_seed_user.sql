aa1a32cfda7979b82623c63dc38f1b7537cea4df
John Kuempel <jkuempel@its4logistics.com>
2022-07-21 17:28:25 -0400

    Seed admin user in database

---
+++ migrations/20220721210857_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users(user_id, username, password_hash)
+VALUES(
+    '81d8e66c-3272-4197-8fc8-e2b894683171',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$SeT4w8XAJl+CeAVlyoJ+cQ$XzYMX6I3ZeJsBsQ5bxEVb13aOEKhD5YXdAdkpyrKhAk'
+);
